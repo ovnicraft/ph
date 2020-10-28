7d993166a53e8ebb569310fb6a9d8b96f59b87db
Cristian Salamea <salamea@vauxoo.com>
2020-10-28 02:04:28 -0500

    [IMP] migration script

---
+++ mig_12.py
@@ -0,0 +1,79 @@
+# usr/bin/env python
+
+import logging
+
+import click
+import click_odoo
+
+logger = logging.getLogger(__name__)
+
+
+modules2uninstall = [
+    "account_financial_report_qweb",
+    "account_group",
+    "account_payment_batch_process",
+    "hr_payslip_monthly_report",
+    "invoice_reports",
+    # "invoice_taxexcent",
+    # "invoice_third_party",
+    "l10n_ec_statement",
+    "l10n_ec_statement_import_move_line",
+    "payment_cancel",
+    "report_stock_picking",
+    "stock_cancel",
+    "stock_picking_internal",
+]
+
+
+def uninstall(env, module_names):
+    modules = env["ir.module.module"].search(
+        [("name", "in", module_names), ("state", "=", "installed")]
+    )
+    logger.info("uninstalling %s", modules.mapped("name"))
+    modules.button_immediate_uninstall()
+    env.cr.commit()
+
+
+def install(env, names):
+    modules = env["ir.module.module"].search(
+        [("name", "in", names), ("state", "=", "uninstalled")]
+    )
+    logger.info("installing %s", modules.mapped("name"))
+    modules.button_immediate_install()
+    env.cr.commit()
+
+
+def remove_view(env):
+    env.cr.execute("delete from ir_ui_view where id = 1169")
+    views = env["ir.ui.view"].search([("arch_db", "ilike", "%auth_number%")])
+    from lxml import etree
+
+    for view in views:
+        doc = etree.fromstring(view.arch_db)
+        for ele in doc.xpath("//field[@name='auth_number']"):
+            ele.getparent().remove(ele)
+
+    env.cr.commit()
+
+
+def update_list(env):
+    env["ir.module.module"].update_list()
+
+
+def upgrade_base(env):
+    mod = env["ir.module.module"].search([("name", "=", "base")])
+    mod.button_immediate_upgrade()
+
+
+@click.command()
+@click_odoo.env_options(with_rollback=False)
+def main(env):
+    remove_view(env)
+    uninstall(env, modules2uninstall)
+    update_list(env)
+    uninstall(env, ["l10_ec_chart"])
+    install(env, ["base_vat"])
+
+
+if __name__ == "__main__":
+    main()
--- upgrade-v12.0.sql
+++ upgrade-v12.0.sql
@@ -1,10 +1,52 @@
-UPDATE ir_module_module SET name = 'hr_payslip_report' WHERE name = 'hr_payslip_monthly_report';
-UPDATE ir_model_data SET module = 'hr_payslip_report' WHERE module = 'hr_payslip_monthly_report';
-UPDATE ir_model_data SET name = 'module_hr_payslip_report'
-       WHERE name = 'module_hr_payslip_monthly_report'
+UPDATE ir_module_module SET name = 'l10n_ec_payment_order' WHERE name = 'l10n_ec_payments';
+UPDATE ir_model_data SET module = 'l10n_ec_payment_order' WHERE module = 'l10n_ec_payments';
+UPDATE ir_model_data SET name = 'module_l10n_ec_payment_order'
+       WHERE name = 'module_l10n_ec_payments'
        AND module = 'base'
        AND model = 'ir.module.module';
-UPDATE ir_module_module_dependency SET name = 'hr_payslip_report'
-       WHERE name = 'hr_payslip_monthly_report';
-UPDATE ir_translation SET module = 'hr_payslip_report'
-       WHERE module = 'hr_payslip_monthly_report';
+UPDATE ir_module_module_dependency SET name = 'l10n_ec_payment_order'
+       WHERE name = 'l10n_ec_payments';
+UPDATE ir_translation SET module = 'l10n_ec_payment_order'
+       WHERE module = 'l10n_ec_payments';
+
+
+UPDATE ir_module_module SET name = 'account_move_cancel' WHERE name = 'l10n_ec_account_move';
+UPDATE ir_model_data SET module = 'account_move_cancel' WHERE module = 'l10n_ec_account_move';
+UPDATE ir_model_data SET name = 'module_account_move_cancel'
+       WHERE name = 'module_account_move_cancel'
+       AND module = 'base'
+       AND model = 'ir.module.module';
+UPDATE ir_module_module_dependency SET name = 'account_move_cancel'
+       WHERE name = 'l10n_ec_account_move';
+UPDATE ir_translation SET module = 'account_move_cancel'
+       WHERE module = 'l10n_ec_account_move';
+
+
+UPDATE ir_module_module SET name = 'l10n_ec_payroll_data' WHERE name = 'payroll_data';
+UPDATE ir_model_data SET module = 'l10n_ec_payroll_data' WHERE module = 'payroll_data';
+UPDATE ir_model_data SET name = 'module_l10n_ec_payroll_data'
+       WHERE name = 'module_l10n_ec_payroll_data'
+       AND module = 'base'
+       AND model = 'ir.module.module';
+UPDATE ir_module_module_dependency SET name = 'l10n_ec_payroll_data'
+       WHERE name = 'l10n_ec_account_move';
+UPDATE ir_translation SET module = 'l10n_ec_payroll_data'
+       WHERE module = 'payroll_data';
+
+
+UPDATE ir_module_module SET name = 'l10n_ec_followup' WHERE name = 'account_followup_ec';
+UPDATE ir_model_data SET module = 'l10n_ec_followup' WHERE module = 'account_followup_ec';
+UPDATE ir_model_data SET name = 'module_l10n_ec_followup'
+       WHERE name = 'module_account_followup_ec'
+       AND module = 'base'
+       AND model = 'ir.module.module';
+UPDATE ir_module_module_dependency SET name = 'l10n_ec_followup'
+       WHERE name = 'account_followup_ec';
+UPDATE ir_translation SET module = 'l10n_ec_followup'
+       WHERE module = 'account_followup_ec';
+
+
+DELETE FROM ir_ui_view where name = 'view.account.payment.line.conciled.form';
+DELETE FROM ir_ui_view where name = 'view.users.restrictions.stocks';
+DELETE FROM ir_ui_view where arch_db ilike '%auth_number%';
+DELETE FROM ir_ui_view WHERE name = 'account.refund.invoice.wizard.form';
