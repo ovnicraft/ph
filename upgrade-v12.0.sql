bfaa03bd945fc3e7ece0b0e99a6a7e4d04015982
Cristian Salamea <salamea@vauxoo.com>
2021-04-01 21:52:24 -0500

    update 12.0

--- upgrade-v12.0.sql
+++ upgrade-v12.0.sql
@@ -46,7 +46,24 @@ UPDATE ir_translation SET module = 'l10n_ec_followup'
        WHERE module = 'account_followup_ec';
 
 
+UPDATE ir_module_module SET name = 'l10n_ec_liq_purchase' WHERE name = 'l10n_ec_epurchase';
+UPDATE ir_model_data SET module = 'l10n_ec_liq_purchase' WHERE module = 'l10n_ec_epurchase';
+UPDATE ir_model_data SET name = 'module_l10n_ec_liq_purchase'
+       WHERE name = 'module_l10n_ec_epurchase'
+       AND module = 'base'
+       AND model = 'ir.module.module';
+UPDATE ir_module_module_dependency SET name = 'l10n_ec_liq_purchase'
+       WHERE name = 'l10n_ec_epurchase';
+UPDATE ir_translation SET module = 'l10n_ec_liq_purchase'
+       WHERE module = 'l10n_ec_epurchase';
+
 DELETE FROM ir_ui_view where name = 'view.account.payment.line.conciled.form';
 DELETE FROM ir_ui_view where name = 'view.users.restrictions.stocks';
 UPDATE ir_ui_view set active = 'f' where arch_db ilike '%auth_number%';
 DELETE FROM ir_ui_view WHERE name = 'account.refund.invoice.wizard.form';
+DELETE FROM ir_ui_view WHERE name ilike '%ir.actions.report.xml%';
+DELETE FROM ir_ui_view where name = 'view.account.invoice.epurchase.form';
+DELETE FROM ir_module_module_dependency WHERE name = 'base_xlsx';
+UPDATE account_invoice
+SET invoice_number = substring(invoice_number,1,3)||'-'||substring(invoice_number, 4,3)||'-'||substring(invoice_number, 7,9)
+WHERE STATE NOT IN ('draft','cancel') ;
